fab6fe39cde47e2e36b7690b2f6ba0303b8a84b4
Rafael Rayco <rayco.rafaeljoseph.05032003@gmail.com>
2025-02-23 20:27:09 +0800

    Comment out App Guide

--- capstone/lib/home_page.dart
+++ capstone/lib/home_page.dart
@@ -2536,16 +2536,16 @@ class OthersPage extends StatelessWidget {
           onTap: _downloadApk, // Call the download function
         ),
         // App Guide
-        ListTile(
-          leading: const Icon(Icons.help_outline, color: Colors.orange),
-          title: const Text('App Guide'),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const AppGuidePage()),
-            );
-          },
-        ),
+        // ListTile(
+        //   leading: const Icon(Icons.help_outline, color: Colors.orange),
+        //   title: const Text('App Guide'),
+        //   onTap: () {
+        //     Navigator.push(
+        //       context,
+        //       MaterialPageRoute(builder: (context) => const AppGuidePage()),
+        //     );
+        //   },
+        // ),
         // Log Out
         ListTile(
           leading: const Icon(Icons.logout, color: Colors.red),
